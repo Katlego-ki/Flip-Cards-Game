57c85f1fe32ecd36d4fb1f795dab752f3350a686
Mailula <katlegobairvin@gmail.com>
2022-08-26 22:12:32 +0200

    Added mouseListener

--- GameEngine.java
+++ GameEngine.java
@@ -1,22 +1,54 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
 
 public class GameEngine extends JPanel implements ActionListener {
+
     public GameEngine(){
         this.setPreferredSize(new Dimension(1000, 800)); //SIZE OF PANEL OR "GAME SCREEN"
         this.setBackground(Color.GRAY);
         this.setFocusable(true);
-        this.addKeyListener(new KeyAdapter() {
+        this.addMouseListener(new MouseListener() {
             @Override
-            public void keyPressed(KeyEvent e) {
-                super.keyPressed(e);
+            public void mouseClicked(MouseEvent e) {
+                /*if(getClickCount() == 2 compare){
+                    compare the 2 selected objects
+
+                    if(object1 == object2){
+                        leave them flipped over
+
+                     } else {
+                        flip them down again.
+
+                        }
+                    }*/
 
 
+                /* Left click to cancel card selection.
+                 */
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                //Zoom in When mouse enters??
+                //Zoom in "New Game" and "Quit" buttons
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                //Zoom out (back to original size)??
             }
         });
 
@@ -25,6 +57,7 @@ public class GameEngine extends JPanel implements ActionListener {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+
 
         for(int k = 0; k < 4; k++){
             for(int i = 0; i < 4; i++){
@@ -42,6 +75,6 @@ public class GameEngine extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        
+
     }
 }
